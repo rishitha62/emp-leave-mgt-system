45486acaa520458ae716d5b826c849d636f37801
rishitha62 <133572634+rishitha62@users.noreply.github.com>
2026-02-24 19:01:00 +0530

    Add UserService: manages users and password encoding

---
+++ src/main/java/com/example/empleavemgtsystem/service/UserService.java
@@ -0,0 +1,5 @@
+package com.example.empleavemgtsystem.service;
+
+import com.example.empleavemgtsystem.entity.User;
+Import com.example.empleavemgtsystem.repository.UserRepository;
+import org.springframework.beans.factory.A
