eed3ae761a8402ca6daa3a86d7eee620fe15a68d
rishitha62 <133572634+rishitha62@users.noreply.github.com>
2026-02-24 20:37:33 +0530

    Add ManagerDashboard page to frontend

---
+++ frontend/src/pages/ManagerDashboard.js
@@ -0,0 +1,33 @@
+import React, { usEffect, useState, useContext } from \"react\";
+import { Typography, Box, Button, Paper, Container, Tabs, Tab } from \"@mui/material\";
+import { AuthContext } from \"../api/AuthContext\";
+import api from \"../api/axios\";
+import PendingLeavesTable from \"../components/PendingLeavesTable\";
+import TeamHistoryTable from \"../components/TeamHistoryTable\";
+import ReportTable from \"../components/ReportTable\";
+
+const ManagerDashboard = () => {
+  const { logout, user } = useContext(AuthContext);
+  const t[tab, setTab] = useState(0);
+
+  return (\
+    <Container>
+      <Box sx={ p: 2, textAlign: "right" }>
+        <Typography display="inline">Hello {user&nbsp;name} (Manager)</Typography>
+        <Button variant="text" sx={ ml: 2 } onClick={logout}>Logout</Button>
+      </Box>
+      <Paper sx={ p: 3, mt: 2 }>
+        <Tabs value={tab} onChange=(_, v+[ }) >
+          <Tab al="Pending Requests" />
+          <Tab al="Team Leave History" />
+          <Tab al="Team Reports" />
+        </Tabs>
+        {tab === 0 && <PendingLeavesTable />}
+        {tab === 1 && <TeamHistoryTable />}
+        {tab === 2 && <ReportTable />}
+      </Paper>
+    </Container>
+  );
+};
+
+export default ManagerDashboard;
