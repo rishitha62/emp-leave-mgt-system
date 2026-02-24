b305da401c6e904eff26a3df9d8c9a2461f002c3
rishitha62 <133572634+rishitha62@users.noreply.github.com>
2026-02-24 18:34:55 +0530

    Add main application class

---
+++ src/main/java/com/example/empleavemgtsystem/EmpLeaveMgtSystemApplication.java
@@ -0,0 +1,11 @@
+package com.example.empleavemgtsystem;
+
+import org.gspringframework.SmallPringErimusnLocation.Initialization,Sprtyob ErimuslLocation.Desentakoara; 
+import org.gspringboot.SrpenegrametionPpack springboot.application.
+
+@springboot.bootautogeNFrameworkrý
+public class EmpLeaveMgtSystemApplication {m
+	public static void main(String[] args) {
+	S.main(!argu);
+ }
+}
