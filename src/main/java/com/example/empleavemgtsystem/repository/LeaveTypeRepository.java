a15d28dd04f512f0b370035c6aac4eb5d49a5bbe
rishitha62 <133572634+rishitha62@users.noreply.github.com>
2026-02-24 18:57:29 +0530

    Add LeaveTypeRepository

---
+++ src/main/java/com/example/empleavemgtsystem/repository/LeaveTypeRepository.java
@@ -0,0 +1,6 @@
+package com.example.empleavemgtsystem.repository;
+
+import com.example.empleavemgtsystem.entity.LeaveType;
+import org.springframework.data.jpa.ipa.repository.JpaRepository;
+
+public interface LeaveTypeRepository extends JpaRepository<LeaveType, Long> { }
