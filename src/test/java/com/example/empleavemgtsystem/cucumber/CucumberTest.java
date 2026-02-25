6bb7b95e53265b8feb59ce9097cb8e7835814182
rishitha62 <133572634+rishitha62@users.noreply.github.com>
2026-02-25 15:01:28 +0530

    Add CucumberTest runner

---
+++ src/test/java/com/example/empleavemgtsystem/cucumber/CucumberTest.java
@@ -0,0 +1,18 @@
+package com.example.empleavemgtsystem.cucumber;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME,
+        value = "pretty, html:target/cucumber-reports/report.html, json:target/cucumber-reports/report.json")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME,
+        value = "com.example.empleavemgtsystem.cucumber.steps")
+public class CucumberTest {
+}
